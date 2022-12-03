2f28268d87892cc5c91254af0731a4016e71fc45
kuriyantoadi <melatikuning212@gmail.com>
2022-12-04 00:45:57 +0700

    update 4 des 2022, update 7

--- application/views/admin/pelanggaran_siswa.php
+++ application/views/admin/pelanggaran_siswa.php
@@ -1,7 +1,7 @@
 <div class="page-wrapper">
     <div class="row page-titles">
         <div class="col-md-5 align-self-center">
-            <h3 class="text-themecolor">Halaman Pelanggaran</h3>
+            <h3 class="text-themecolor">Halaman Admin</h3>
         </div>
     </div>
     <div class="container-fluid">
@@ -13,48 +13,54 @@
 
                         <?php
                         $no = 1;
-                        foreach ($pelanggaran_siswa as $row) {
+
+                        if (empty($pelanggaran_siswa)) {
+                            echo "Siswa Ini tidak memiliki catatan pelanggaran";
+                        } else {
+                            foreach ($pelanggaran_siswa as $row) {
+
                         ?>
 
-                            <h5 style="margin-top: 30px;">Pelanggaran ke- <?= $no++ ?></h5>
+                                <h5 style="margin-top: 30px;">Pelanggaran ke- <?= $no++ ?></h5>
 
-                            <table class="table table-bordered">
+                                <table class="table table-bordered">
 
-                                <tr>
-                                    <td>Nama Siswa</td>
-                                    <td><?= $row->nama_siswa ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Kelas</td>
-                                    <td><?= $row->tingkatan . ' ' . $row->jurusan . ' ' . $row->kode_kelas ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Nama Point</td>
-                                    <td><?= $row->nama_point ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Guru BK</td>
-                                    <td><?= $row->nama ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tanggal Melanggar</td>
-                                    <td><?= $row->tgl_kejadian ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tanggal Input</td>
-                                    <td><?= $row->tgl_input ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tanggal Input</td>
-                                    <td><?= $row->tgl_input ?></td>
-                                </tr>
-                                <tr>
-                                    <td class="col-4">Photo Pelanggaran</td>
-                                    <td>
-                                        <img width="200px" src="<?= base_url() ?>assets/photo_pelanggaran/<?= $row->photo_pelanggaran ?>" alt="Photo">
-                                    </td>
-                                </tr>
-                            </table>
+                                    <tr>
+                                        <td>Nama Siswa</td>
+                                        <td><?= $row->nama_siswa ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Kelas</td>
+                                        <td><?= $row->tingkatan . ' ' . $row->jurusan . ' ' . $row->kode_kelas ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Nama Point</td>
+                                        <td><?= $row->nama_point ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Guru BK</td>
+                                        <td><?= $row->nama ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tanggal Melanggar</td>
+                                        <td><?= $row->tgl_kejadian ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tanggal Input</td>
+                                        <td><?= $row->tgl_input ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tanggal Input</td>
+                                        <td><?= $row->tgl_input ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-4">Photo Pelanggaran</td>
+                                        <td>
+                                            <img width="200px" src="<?= base_url() ?>assets/photo_pelanggaran/<?= $row->photo_pelanggaran ?>" alt="Photo">
+                                        </td>
+                                    </tr>
+                                </table>
+                            <?php } ?>
                         <?php } ?>
                         <div class="text-xs-right">
                             <!-- <a href="<?= site_url('Admin/siswa_detail/' . $row->id_siswa) ?>" class="btn btn-sm btn-inverse">Kembali</a> -->
--- application/views/admin/prestasi_siswa.php
+++ application/views/admin/prestasi_siswa.php
@@ -13,54 +13,58 @@
 
                         <?php
                         $no = 1;
-                        foreach ($tampil_prestasi as $row) {
+                        if (empty($tampil_prestasi)) {
+                            echo "Siswa Ini tidak memiliki catatan prestasi";
+                        } else {
+                            foreach ($tampil_prestasi as $row) {
                         ?>
 
-                            <h5 style="margin-top: 30px;">Prestasi ke- <?= $no++ ?></h5>
+                                <h5 style="margin-top: 30px;">Prestasi ke- <?= $no++ ?></h5>
 
-                            <table class="table table-bordered">
+                                <table class="table table-bordered">
 
-                                <tr>
-                                    <td>Nama Siswa</td>
-                                    <td><?= $row->nama_siswa ?></td>
-                                </tr>
+                                    <tr>
+                                        <td>Nama Siswa</td>
+                                        <td><?= $row->nama_siswa ?></td>
+                                    </tr>
 
-                                <tr>
-                                    <td>Tanggal Pelaksanaan</td>
-                                    <td><?= $row->tanggal_pelaksanaan ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Nama Kegiatan</td>
-                                    <td><?= $row->nama_kegiatan ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Juara ke</td>
-                                    <td><?= $row->juara_ke ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tingkat</td>
-                                    <td><?= $row->tingkat ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tempat Lomba</td>
-                                    <td><?= $row->tempat_lomba ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Tim/Individu</td>
-                                    <td><?= $row->tim_individu ?></td>
-                                </tr>
+                                    <tr>
+                                        <td>Tanggal Pelaksanaan</td>
+                                        <td><?= $row->tanggal_pelaksanaan ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Nama Kegiatan</td>
+                                        <td><?= $row->nama_kegiatan ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Juara ke</td>
+                                        <td><?= $row->juara_ke ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tingkat</td>
+                                        <td><?= $row->tingkat ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tempat Lomba</td>
+                                        <td><?= $row->tempat_lomba ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tim/Individu</td>
+                                        <td><?= $row->tim_individu ?></td>
+                                    </tr>
 
-                                <tr>
-                                    <td>Penyelenggara Acara </td>
-                                    <td><?= $row->penyelenggara_acara  ?></td>
-                                </tr>
-                                <tr>
-                                    <td class="col-4">Photo Prestasi</td>
-                                    <td>
-                                        <img width="200px" src="<?= base_url() ?>assets/photo_prestasi/<?= $row->photo_prestasi ?>" alt="Photo">
-                                    </td>
-                                </tr>
-                            </table>
+                                    <tr>
+                                        <td>Penyelenggara Acara </td>
+                                        <td><?= $row->penyelenggara_acara  ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-4">Photo Prestasi</td>
+                                        <td>
+                                            <img width="200px" src="<?= base_url() ?>assets/photo_prestasi/<?= $row->photo_prestasi ?>" alt="Photo">
+                                        </td>
+                                    </tr>
+                                </table>
+                            <?php } ?>
                         <?php } ?>
                         <div class="text-xs-right">
                             <!-- <a href="<?= site_url('Admin/siswa_detail/' . $row->id_siswa) ?>" class="btn btn-sm btn-inverse">Kembali</a> -->
