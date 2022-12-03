edd1df74813e1f1ee6ea09d231d8ffeb2da7db2a
kuriyantoadi <melatikuning212@gmail.com>
2022-12-03 23:56:39 +0700

    update 3 desember 2022, update 4

--- application/views/admin/pelanggaran_siswa.php
+++ application/views/admin/pelanggaran_siswa.php
@@ -11,7 +11,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Daftar Pelanggaran Siswa</h4>
 
-                        <?php
+                        <?=
                         $no = 1;
                         foreach ($pelanggaran_siswa as $row) {
                         ?>
