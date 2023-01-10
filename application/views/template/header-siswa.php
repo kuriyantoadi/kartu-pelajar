eac5e50a8b5927d4cb4e79e61e4c1e5deb73a2d1
kuriyantoadi <melatikuning212@gmail.com>
2023-01-10 09:54:12 +0700

    update 10 jan 2023, update 6

--- application/views/template/header-siswa.php
+++ application/views/template/header-siswa.php
@@ -30,7 +30,7 @@ $ses_id = $this->session->userdata('ses_id');
         <header class="topbar">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="<?= base_url() ?>/Siswa">
+                    <a class="navbar-brand" href="<?= base_url() ?>Siswa">
                         <b>
                             <img src="<?= base_url() ?>assets/images/logosmk.png" width="50" alt="homepage" class="dark-logo" />
                             <img src="<?= base_url() ?>assets/images/logosmk.png" width="50" alt="homepage" class="light-logo" />
